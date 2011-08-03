a058656c433b00e02ddd8112c93565130aa99e31
Joe White <joe@rjdj.me>
2011-08-03 19:47:30 +0100

    Added MainViewController

---
+++ ZenGarden_GUI/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  ZenGarden_GUI
+//
+//  Created by Joe White on 03/08/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface MainViewController : NSViewController {
+@private
+    
+}
+
+@end
---
+++ ZenGarden_GUI/MainViewController.m
@@ -0,0 +1,29 @@
+//
+//  MainViewController.m
+//  ZenGarden_GUI
+//
+//  Created by Joe White on 03/08/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+
+@implementation MainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
